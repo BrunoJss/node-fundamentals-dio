8b55a76d3a56a72ed9b8fb72e3d6d7c86a4e3a4d
brunojss <bruno.jsscruz@gmail.com>
2025-08-10 13:50:30 -0300

    criando assinaturas dos métodos da entidade carrinho

---
+++ 06-shopee-cart/src/services/cart.js
@@ -0,0 +1,9 @@
+//quais acoes meu carrinho pode fazer
+//adicionar item
+async function addItem(userCart){}
+//deletar item
+async function deleteItem(userCart, nameItem){}
+//remover um item - diminui item
+async function removeItem(userCart, indexItem){}
+//calcular total
+async function calculateTotal(userCart) {}
