cfb5043778d0870ed2b2e81a688523aca2415895
brunojss <bruno.jsscruz@gmail.com>
2025-08-10 14:08:59 -0300

    implementando funções para adicionar itens ao carrinho e para calcular o total do carrinho

--- 06-shopee-cart/src/services/cart.js
+++ 06-shopee-cart/src/services/cart.js
@@ -1,9 +1,21 @@
 //quais acoes meu carrinho pode fazer
 //adicionar item
-async function addItem(userCart){}
+async function addItem(userCart, item){
+    userCart.push(item)
+}
+async function calculateTotal(userCart) {
+    return userCart.reduce((total, item) => total + item.subtotal(), 0)
+}
+
 //deletar item
 async function deleteItem(userCart, nameItem){}
 //remover um item - diminui item
 async function removeItem(userCart, indexItem){}
 //calcular total
-async function calculateTotal(userCart) {}
+
+export {
+    addItem,
+    calculateTotal,
+    deleteItem,
+    removeItem,
+}
