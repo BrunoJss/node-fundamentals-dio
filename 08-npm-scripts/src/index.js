f30d74ba7bcb49d063ac4a78933f9c099d858a01
brunojss <bruno.jsscruz@gmail.com>
2025-08-18 20:11:41 -0300

    Instalação de pacotes externos com NPM

--- 08-npm-scripts/src/index.js
+++ 08-npm-scripts/src/index.js
@@ -1,2 +1,11 @@
 console.log("Hello estou rodando via script")
 console.log("Iniciando servidor")
+
+
+/*
+Nomenclatura Scripts
+
+prebuild => npm install
+start => node src/index.js
+start:watch => node --watch src/index.js
+*/
