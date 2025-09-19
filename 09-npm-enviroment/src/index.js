297da86bfb585f2ff0432fd8dfc56315618f940b
brunojss <bruno.jsscruz@gmail.com>
2025-09-18 21:09:03 -0300

    Utilização de multiplos .env

--- 09-npm-enviroment/src/index.js
+++ 09-npm-enviroment/src/index.js
@@ -1,6 +1,8 @@
 import connectToDatabase from "./database/data.js";
 
 
+//para carregar o .env no node é necessário usar a flag "--env-file=". Ex. package.json 
+
 async function main() {
     await connectToDatabase(process.env.USERDATABASE, process.env.PASSDATABASE);
 }
